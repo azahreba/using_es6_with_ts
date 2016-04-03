ff06be6d694bb5e3f180f09e375004af11db1435
Artur Zahreba <azahreba@gmail.com>
2016-04-03 23:29:54 +0200

    explore features of TS

--- hw.js
+++ hw.js
@@ -1,6 +1,18 @@
-let a = [1, 2, 3];
-let b = a.map(dig => dig * dig);
-for (let c = 1; c < 10; c++) {
-    console.log(c);
-}
+const obj = { a: 1 };
+obj.a = 2;
+var tsNameSpace;
+(function (tsNameSpace) {
+    tsNameSpace.a = 1;
+})(tsNameSpace || (tsNameSpace = {}));
+tsNameSpace.a = 3;
+const greetArrow = (name) => {
+    return "Hello, " + name;
+};
+console.log(greetArrowTs('Dead zone'));
+const greetArrowTs = (name) => "Hello, " + name;
+const address = {
+    city: 'NY',
+    zip: 12345,
+};
+let { city: c } = address;
 //# sourceMappingURL=hw.js.map
--- hw.ts
+++ hw.ts
@@ -1,6 +1,51 @@
-let a = [1,2,3];
-let b = a.map(dig => dig*dig);
+/*
+*
+* const vs namespace
+*
+* */
 
-for (let c = 1; c < 10; c++) {
-    console.log(c);
+const obj = {a: 1};
+obj.a = 2; //sets a prop to 2;
+
+namespace tsNameSpace {
+    export const a = 1;
 }
+tsNameSpace.a = 3; //Error
+
+/*
+*
+* let/const function + annotate params
+*
+* */
+
+const greetArrow = (name/*Error: param has any type */) => {
+    return "Hello, " + name;
+};
+
+console.log(greetArrowTs('Dead zone'));
+
+//by let or const TS can manage dead zones
+//can't bind this with arrow function
+//arrow function isn't new-able
+const greetArrowTs = (name: string) => "Hello, " + name;
+
+/*
+*
+* Destructuring
+*
+* interface objects that will be exported
+*
+* */
+
+interface USPostalAddress {
+    city: string,
+    zip: number,
+    state?: string //optional
+}
+
+const address : USPostalAddress = {
+    city: 'NY',
+    zip: 12345,
+};
+
+let {city:/*as*/ c} = address;
